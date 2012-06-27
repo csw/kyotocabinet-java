315bd8d24e913bc1e33c2830cbb03b56e7234c5a
Clayton Wheeler <cswh@umich.edu>
2012-06-26 21:10:45 -0400

    Added test for get returning nil.

--- spec/kyotocabinet_spec.rb
+++ spec/kyotocabinet_spec.rb
@@ -11,6 +11,22 @@ module KyotoCabinet
       db.count.should == 1
     end
 
+    describe "with in-memory DB" do
+      before(:each) do
+        @db = DB.new
+        @db.open("%", KyotoCabinet::DB::OWRITER | KyotoCabinet::DB::OCREATE)
+        @db["hello"] = "world"
+      end
+
+      it "returns nil from get properly" do
+        @db.get("foo").should be_nil
+      end
+
+      after(:each) do
+        @db.close
+      end
+    end
+
   end
 
 end
